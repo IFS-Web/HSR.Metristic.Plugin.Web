fb65bbf9e626e97824d466a166e81ec3d8a9fa90
Wasabideveloper <tobias.blaser@hsr.ch>
2016-10-05 15:53:10 -0300

    Update gulpfile & tsd to compile & deploy

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+export * from "./web/css-metric/css-metric";
+export * from "./web/html-metric/html-metric";
+export * from "./web/html-w3c-validator/html-w3c-validator";
+export * from "./web/js-style-check/js-style-check";
