52f700ce53cd449c80ec411c88266e8daabc9fbe
Wasabideveloper <tobias.blaser@hsr.ch>
2016-10-19 16:38:59 +0200

    Implement page rendering by phantomjs

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./web/css-metric/css-metric";
 export * from "./web/html-metric/html-metric";
 export * from "./web/html-w3c-validator/html-w3c-validator";
 export * from "./web/js-style-check/js-style-check";
+export * from "./web/page-visualizer/page-visualizer";
